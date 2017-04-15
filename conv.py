937218710cd1313d82fa69891810e4d281f5e64a
Jordan Ott <jordanott365@gmail.com>
2017-04-15 00:12:54 -0400

    Add files via upload

---
+++ conv.py
@@ -0,0 +1,96 @@
+import numpy as np
+
+x = np.random.rand(28,28)
+
+class conv_layer():
+	def __init__(self,filter_dim,num_filters,stride):
+		self.filter_dim = filter_dim
+		self.num_filters = num_filters
+		self.stride = stride
+		self.filters = self.init_filters()
+
+	def init_filters(self):
+		return np.random.normal(size=(self.num_filters,self.filter_dim,self.filter_dim))
+
+	def add_padding(self,layer_input):
+		# padd image with zeros
+		return np.lib.pad(layer_input, (1,1), 'constant', constant_values=(0))
+
+	def conv(self,layer_input):
+
+		# output of conv layer is same dimension as input with a depth of the number of filters
+		layer_output = np.zeros((self.num_filters,layer_input.shape[1],layer_input.shape[2]))
+
+		# pad each dimension of input with zeros
+		for dim in range(layer_input.shape[0]):
+			layer_input = self.add_padding(layer_input[dim])
+
+		for filter_num in range(self.num_filters): 
+			for start_row in range(0,layer_output.shape[1],self.stride):
+				for start_col in range(0,layer_output.shape[2],self.stride):
+					layer_output[filter_num,start_row,start_col] = np.sum(self.filters[filter_num]*
+						layer_input[start_row:start_row+self.filter_dim, start_col:start_col+self.filter_dim])
+		
+		return layer_output
+
+	def backprop(self):
+		pass
+
+
+class max_pool_layer():
+	def __init__(self,pool_size,stride):
+		self.pool_size = pool_size
+		self.stride = stride
+
+	def max_pool(self,layer_input):
+		out_dim1 = (layer_input.shape[1]-self.pool_size)/self.stride + 1
+		out_dim2 = (layer_input.shape[2]-self.pool_size)/self.stride + 1
+		print out_dim1
+		# output of conv layer is same dimension as input with a depth of the number of filters
+		layer_output = np.zeros((layer_input.shape[0],out_dim1,out_dim2))
+
+		print layer_output.shape
+
+x = x.reshape((1,28,28))
+
+# 3x3 filter, filter one, stride one
+conv1 = conv_layer(3,1,1)
+conv2 = conv_layer(3,1,1)
+
+out1 = conv1.conv(x)
+print out1.shape
+out2 = conv2.conv(out1)
+print out2.shape
+
+pool1 = max_pool_layer(2,1)
+pool1.max_pool(out2)
+
+# x = np.array(
+# [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,],
+# [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,],
+# [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,],
+# [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,],
+# [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,],
+# [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,],
+# [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,],
+# [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,],
+# [0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,],
+# [0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,],
+# [0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,],
+# [0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,],
+# [0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,],
+# [0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,],
+# [0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,],
+# [0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,],
+# [0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,],
+# [0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,],
+# [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,],
+# [0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,],
+# [0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,],
+# [0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,],
+# [0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,],
+# [0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,],
+# [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,],
+# [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,],
+# [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,],
+# [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,]])
