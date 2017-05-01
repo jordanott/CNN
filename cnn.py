Fixed conv backprop

cnn.py
@@ -10,9 +10,8 @@ cnn.add_layer('input',shape=(28,28,1))
 # cnn.add_layer('fc',num_neurons=32,activation='relu')
 cnn.add_layer('conv',stride=1,num_filters=3,filter_dim=3,padding=1,activation='relu')
 cnn.add_layer('max_pool',stride=1,pool_size=2)
-
-
-
+cnn.add_layer('conv',stride=1,num_filters=3,filter_dim=3,padding=1,activation='relu')
+cnn.add_layer('max_pool',stride=1,pool_size=2)
 cnn.add_layer('output',num_neurons=10,activation='softmax')
 
 
@@ -31,7 +30,7 @@ for i in range(0,len(train_set[0])):
 	data = train_set[0][i].reshape((28,28,1))
 	target = np.zeros((1,10))
 	target[0,train_set[1][i]] = 1
-	
+
 	# backprop through network
 	predictions = cnn.forward(data)
 	#print "PREDICTIONS:",predictions
@@ -39,18 +38,18 @@ for i in range(0,len(train_set[0])):
 
 	gradient = cnn.cross_entropy_gradient(predictions,target)
 	# TODO: fix loss
-	# loss += cnn.get_cost(predictions,target)		
+	# loss += cnn.get_cost(predictions,target)
 	cnn.backward(gradient)
 	if target[0,prediction]:
 		correct += 1
-	
+
 	#print "Loss:",loss
 
 	if (i+1) % 128 == 0:
 		print "Epoch",i//128
 		print "Accuracy",correct/128.0
 		correct = 0
-		
+
 		gradient = np.zeros((1,10))
 
 correct = 0
@@ -58,7 +57,7 @@ for i in range(0,len(test_set[0])):
 	data = test_set[0][i].reshape((28,28,1))
 	target = np.zeros((1,10))
 	target[0,test_set[1][i]] = 1
-	
+
 	# backprop through network
 	predictions = cnn.forward(data)
 	#print "PREDICTIONS:",predictions
@@ -66,5 +65,5 @@ for i in range(0,len(test_set[0])):
 
 	if target[0,prediction]:
 		correct += 1
-	
+
 print "Test accuracy:", correct/float(len(test_set[0]))

layers/conv_layer.py
@@ -52,16 +52,16 @@ class conv_layer():
 		# padd image with zeros
 		padded = np.zeros((layer_input.shape[0]+ 2*self.padding,layer_input.shape[1]+2*self.padding,layer_input.shape[2]))
 		padded[self.padding:self.padding+layer_input.shape[0],self.padding:self.padding+layer_input.shape[0]] = layer_input
-		
+
 		return padded
-	
+
 	def forward(self,layer_input):
 		# initialize filter update matrix
 		self.filter_updates = np.zeros(self.filters.shape)
 		# output of conv layer is same dimension as input with a depth of the number of filters
 		layer_output = np.zeros(self.output_shape)
 
-		if self.padding > 0:	
+		if self.padding > 0:
 			# pad input with zeros
 			self.layer_input_padded = self.add_padding(layer_input)
 		else:
@@ -71,7 +71,7 @@ class conv_layer():
 		for filter_num in range(self.num_filters):
 			for start_row in range(0,layer_output.shape[0],self.stride):
 				for start_col in range(0,layer_output.shape[1],self.stride):
-				
+
 					layer_output[start_row,start_col,filter_num] = np.sum(
 						self.layer_input_padded[start_row:start_row+self.filter_dim, start_col:start_col+self.filter_dim]*
 						self.filters[filter_num])
@@ -85,31 +85,22 @@ class conv_layer():
 		for f in self.filters:
 			reg += np.sum(f*f)
 		return reg
-		
+
 	def backprop(self,gradient):
 		gradient = self.backtivation(self.layer_product) * gradient
 		dLdw = np.zeros(self.layer_input_padded.shape,dtype=np.float64)
-		
+
 		for dim in range(self.num_filters):
 			for start_row in range(0,gradient.shape[0],self.stride):
 				for start_col in range(0,gradient.shape[1],self.stride):
-					dLdw[start_row:start_row+self.filter_dim, start_col:start_col+self.filter_dim] = gradient[start_row,start_col,dim]*self.filters[dim]
+
+					dLdw[start_row:start_row+self.filter_dim, start_col:start_col+self.filter_dim] += gradient[start_row,start_col,dim]*self.filters[dim]
 
 					self.filter_updates[dim] += gradient[start_row,start_col,dim] * self.layer_input_padded[
 		 			start_row*self.stride:start_row*self.stride + self.filter_dim,
 		 			start_col*self.stride:start_col*self.stride + self.filter_dim]
 
-		# for start_row in range(self.filter_dim):
-		# 	for start_col in range(self.filter_dim):
-		# 		for dim in range(self.num_filters):	
-		# 			self.filter_updates[dim] += gradient[start_row,start_col,dim] * self.layer_input_padded[
-		# 			start_row*self.stride:start_row*self.stride + self.filter_dim,
-		# 			start_col*self.stride:start_col*self.stride + self.filter_dim]
-					
-		# 			dLdw[start_row*self.stride:start_row*self.stride + self.filter_dim,
-		# 			start_col*self.stride:start_col*self.stride + self.filter_dim] += gradient[start_row,start_col,dim] * self.filters[dim]
-
 		for filter_num in range(self.num_filters):
 			self.filters[filter_num] += -(self.learning_rate*self.filter_updates[filter_num] + 1e-3 * self.filters[filter_num])
-
-		return dLdw[:,self.padding:dLdw.shape[1]-2*self.padding +1,self.padding:dLdw.shape[2]-2*self.padding+1]
+		
+		return dLdw[self.padding:dLdw.shape[0]-2*self.padding +1,self.padding:dLdw.shape[1]-2*self.padding+1]